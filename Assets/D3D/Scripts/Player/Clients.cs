016ada1899e984f04964a7557137917771a71eb4
Ianmring <Ianmring@gmail.com>
2018-10-15 14:53:49 -0700

    Revert "new Scene"
    This reverts commit 86666783ab177879babcbdb68e665f49cbe7d8a1.

--- Assets/D3D/Scripts/Player/Clients.cs
+++ Assets/D3D/Scripts/Player/Clients.cs
@@ -17,6 +17,7 @@ public class Clients : MonoBehaviour
 
    public float Pay;
 
+    
 
    public float timetocomplete;
 
@@ -71,14 +72,16 @@ public class Clients : MonoBehaviour
         namenum = Random.Range(1, mann.Client.Length);
         Deathnum = Random.Range(1, mann.Death.Length);
 
+<<<<<<< HEAD
      
         clientName.text = ("Name:\n"+mann.Client[namenum]);
         Causeofdeath.text = ("Cause of Death:\n"+ mann.Death[Deathnum]);
-
+=======
         client = mann.Client[namenum];
-        clientName.text = ("Name:\n"+client);
+        name.text = ("Name:\n"+client);
         Reasonofdeath = mann.Death[Deathnum];
         Causeofdeath.text = ("Cause of Death:\n"+ Reasonofdeath);
+>>>>>>> 748012388b21bd9e6a24ce1b6dedb3ff22a396f2
                           
         mann.openclients.Insert(0, this);
         payr.text =("Pay: " + Pay.ToString());
