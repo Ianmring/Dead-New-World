236da32b10f715764114a35bc3b08d40109462d2
BluePaladin <griffymp@yahoo.com>
2019-02-07 07:28:35 -0800

    New Test Scene
    it begins Ui being set up
    client menu setup
    will do more

---
+++ Assets/Lewis Test items/ClientCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientCreator : MonoBehaviour
+{
+    public Transform prefab;
+    public int prefabNumber;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefabNumber = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(prefabNumber <= 0)
+        {
+            
+            Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+            prefabNumber++;
+        }
+    }
+}
---
+++ Assets/Lewis Test items/UIMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using D3D;
+
+public class UIMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Text clientName;
+
+    [SerializeField]
+    private ValueStore store;
+
+    public Clients currentcli;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        store = FindObjectOfType<ValueStore>();
+    }
+
+    // Update is called once per frame
+    public void AssignClient(string newText)
+    {
+        clientName.text = newText;
+    }
+
+    public void GetClient()
+    {
+        store.currentClientSelected = clientName.text;
+        store.currentClient = currentcli;
+    }
+}
