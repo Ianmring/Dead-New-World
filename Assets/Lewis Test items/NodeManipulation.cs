53e803663e922881259774ac2f8ae98a7192318c
BluePaladin <griffymp@yahoo.com>
2019-03-14 11:36:06 -0700

    Nodes nodes Nodes
    created an empty game objecct  and script
    script not finished errors

---
+++ Assets/Lewis Test items/NodeManipulation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManipulation : MonoBehaviour
+{
+    float nodeDistance = 0.0f;
+    GridGraph node;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       node = AstarPath.active.data.gridGraph;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        node.GetNodes(nodeDistance =>
+        {
+            Debug.Log("I found many" + (Vector3)nodeDistance.position);
+        });
+    }
+}
